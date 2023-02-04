8ab0fddf54270afb5f4d0189d33d62c2f28e0266
Aleicer <aleicer.vesga@gmail.com>
2023-02-04 11:05:23 -0500

    Modifique el home, agregue estilos css y barra de navegacion

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import {useNavigate} from "react-router-dom";
-import './home.css';
+import React from 'react'
+import {useNavigate} from 'react-router-dom'
+import logo from '../../assets/top-info-games_logo3.png'
+import imgHome from '../../assets/58670.svg'
+import './home.css'
 
 export const Home = () => {
 
@@ -8,9 +10,41 @@ export const Home = () => {
 
   return (
     <>
-        <h1>A donde deseas dirigirte</h1>
+        {/* <h1>A donde deseas dirigirte</h1>
         <button onClick={() => history('/app-gif')}>App of gif</button>
-        <button onClick={() => history('/info-games')}>Info-Games</button>
+        <button onClick={() => history('/info-games')}>Info-Games</button> */}
+        <header>
+          <div className='container__menu'>
+            <div className='logo'>
+              <img src={logo} alt=''/>
+            </div>
+            <div className='menu'>
+              <nav>
+                <ul>
+                  <li><a href="#">Inicio</a></li>
+                  <li><a href="#">Gift</a></li>
+                  <li><a href="#">Juegos</a></li>
+                  <li><a href="#">Perfil</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </header>
+        <main>
+          <div className="container__cover">
+            <div className="cover">
+              <div className="text">
+                <h1>Top Info Games</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos fuga ipsam saepe cupiditate assumenda fugiat eos ducimus expedita asperiores sed.</p>
+
+                <input type='button' value='Leer Más' />
+              </div>
+              <div className="svg">
+                <img src={imgHome} alt='' />
+              </div>
+            </div>
+          </div>
+        </main>
     </>
   )
 }
